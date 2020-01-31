b7fe9e0043e01b2bb5702e7cf52c329779173832
Sarah Gill <gills@uchicago.edu>
2020-01-31 09:32:36 -0600

    Update explore_pm25.R
    added a couple regressions that don't work

--- analysis/explore_pm25.R
+++ analysis/explore_pm25.R
@@ -20,6 +20,7 @@ pm25_chicago %>%
   ggplot(aes(x = site_name, y =  mean_pm25))+
     geom_bar(stat = "identity")
 
+#line plot of avg monthly pm25 for a single station
 plot_month_pm25 <- function(xi, s_name){
 one_site <- pm25_chicago%>%
   filter(site_id == xi)
@@ -37,13 +38,34 @@ one_site %>%
 p1 <- plot_month_pm25(170310052,"MAYFAIR PUMP STATION")
 
 p2<- plot_month_pm25(170310057, "SPRINGFIELD PUMP STATION")
-#plot_month_pm25(170311016, "VILLAGE HALL")
 
-p3 <- plot_month_pm25(181270024, "Ogden Dunes- Water Treatment Plant")
+p3 <- plot_month_pm25(170311016, "VILLAGE HALL")
 
-p4<- plot_month_pm25(550590019, "CHIWAUKEE PRAIRIE STATELINE")
+p4 <- plot_month_pm25(181270024, "Ogden Dunes- Water Treatment Plant")
+
+#plot_month_pm25(550590019, "CHIWAUKEE PRAIRIE STATELINE")
 
 grid.arrange(p1,p2,p3,p4)
 
 
+#doesn't work
+regn_month_pm25 <- function(xi){
+  one_site <- pm25_chicago%>%
+    filter(site_id == xi)
+  one_site %>%
+    group_by(month=floor_date(date, "month")) %>%
+    #https://ro-che.info/articles/2017-02-22-group_by_month_r
+    summarize(mean_pm25 = mean(daily_mean_pm_2_5_concentration)) %>%
+    
+    lin_model <- lm(mean_pm25~month)
+    summary(lin_model)
+}
+regn_month_pm25(181270024)
 
+  
+m <- lm(daily_mean_pm_2_5_concentration~date, data = pm25_chicago)
+summary(m)
+  
+  
+  
+  
